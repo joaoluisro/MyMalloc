42b2cceff42b761d17ddcfd10abdfbc86f47a421
João <joaoluisok@gmail.com>
2019-12-04 20:35:29 -0300

    fusao de nos ex 1.a

---
+++ ex_1a/ex_1a.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+extern void iniciaAlocador();
+extern void finalizaAlocador();
+extern void* alocaMem(long int x);
+extern void* alturabrk();
+extern void desalocaMem(void *x);
+
+void imprime(void *x){
+  printf("ender : %x\n",x);
+  printf("prox = %x\n", *(long int*)(x - sizeof(long int)));
+  printf("tam = %ld\n", *(long int*)(x - 2*sizeof(long int)));
+  printf("val = %ld\n", *(long int*)(x - 3*sizeof(long int)));
+  printf("\n");
+}
+
+int main(){
+  void *x;
+  void *b;
+  void *z;
+  void *y;
+  printf("\n");
+  iniciaAlocador();
+  x = alocaMem(8);
+  imprime(x);
+  y = alocaMem(16);
+  imprime(y);
+  z = alocaMem(24);
+  imprime(z);
+  void *u = alocaMem(50);
+  desalocaMem(y);
+  desalocaMem(z);
+
+  imprime(*(long int*)(x - 8));
+  b = alocaMem(12);
+  imprime(b);
+  finalizaAlocador();
+  printf("\n");
+}
---
+++ ex_1a/ex_1a.s
@@ -0,0 +1,131 @@
+.section .data
+  topoInicialHeap: .quad 0
+  head: .quad 0
+  tail: .quad 0
+.section .text
+.globl _start
+.globl iniciaAlocador
+iniciaAlocador:
+  pushq %rbp
+  movq %rsp, %rbp
+  call alturabrk
+  movq %rax, topoInicialHeap
+  popq %rbp
+  ret
+
+.globl finalizaAlocador
+finalizaAlocador:
+  pushq %rbp
+  movq %rsp, %rbp
+  movq topoInicialHeap, %rdi
+  movq $12, %rax
+  syscall
+  popq %rbp
+  ret
+
+.globl alocaMem
+alocaMem:
+  pushq %rbp
+  movq %rsp, %rbp
+  movq %rdi, %rdx
+  jmp verifica
+
+  aloca_fim:
+    call alturabrk        # pega valor antigo de brk
+
+    movq %rax, %rsi        # rsi <- valor brk antigo
+    addq %rdx, %rax
+    add $24, %rax
+    movq %rax, %rdi
+    movq $12, %rax
+    syscall              # arranja espaço
+
+    movq %rsi, %rax
+    movq $1, (%rax)      # validez = 1
+    movq %rdx, 8(%rax)    # tamanho_bloco = tamanho
+    movq %rsi, %rbx
+    addq $48, %rbx
+    addq %rdx, %rbx
+    movq %rbx, 16(%rax)  # proximo_bloco = proximo
+
+    movq %rsi, %rax       # retorna primeiro end.
+    addq $24, %rax
+
+    cmpq $0, head
+    jne adiciona
+    movq %rax, head
+    adiciona:
+      movq %rax, tail
+
+    popq %rbp
+    ret
+
+  aloca_meio:
+    movq %rsi ,%rax
+    movq %rdx, -16(%rsi)
+    movq $1, -24(%rsi)
+    popq %rbp
+    ret
+
+  verifica:
+    call alturabrk
+    cmpq topoInicialHeap, %rax   # verifica se a lista é vazia
+    jne itera
+    jmp aloca_fim
+
+  itera:
+    movq head, %rsi
+    while:
+      cmpq $0, -24(%rsi)
+      jne continue
+      cmpq %rdi, -16(%rsi)
+      jge aloca_meio
+      continue:
+        cmpq %rsi, tail
+        je aloca_fim
+        movq -8(%rsi), %rsi
+
+      jmp while
+
+.globl desalocaMem
+desalocaMem:
+  pushq %rbp
+  movq %rsp, %rbp
+  movq $0, -24(%rdi)
+  movq head, %rsi
+  while_ext:
+    cmpq tail, %rsi
+    je fim_while_ext
+    cmpq $0, -24(%rsi)
+    je fusao_nos
+    pos_fusao:
+      movq -8(%rsi), %rsi
+      jmp while_ext
+  fim_while_ext:
+  popq %rbp
+  ret
+
+  fusao_nos:
+    movq -8(%rsi), %rcx
+    movq -16(%rsi), %r9
+    while_nos:
+      cmpq $0, -24(%rcx)
+      jne fim_while_nos
+      addq -16(%rcx), %r9
+      movq -8(%rcx), %rcx
+      jmp while_nos
+    fim_while_nos:
+      movq %r9, -16(%rsi)
+      movq %rcx, -8(%rsi)
+    jmp pos_fusao
+
+
+.globl alturabrk
+alturabrk:
+  pushq %rbp
+  movq %rsp, %rbp
+  movq $12, %rax
+  movq $0, %rdi
+  syscall
+  popq %rbp
+  ret
